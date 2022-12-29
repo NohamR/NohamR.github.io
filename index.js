7e1a491206d8b7f2eb4178aa32e524e122884d16
√(noham)² <noham.rivoirard@gmail.com>
2022-12-29 23:52:31 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var counter = document.getElementById('conteur-de-vues')
 
 const updateCounter = async () => {
-    const data = await fetch('https://api.countapi.xyz/hit/nohamr-counter/visits')
+    const data = await fetch('https://api.countapi.xyz/hit/nohamr-counter/visits-v1')
     const count = await data.json()
     console.log(count.value)
     document.getElementById("compteur-de-vues").innerHTML = count.value
