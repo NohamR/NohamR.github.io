05da5880c8ef522ff5654be5498e34e1cd82daba
√(noham)² <noham.rivoirard@gmail.com>
2023-01-02 13:43:08 +0100

    changes

---
+++ index2.js
@@ -0,0 +1,11 @@
+var counter = document.getElementById('conteur-de-vues')
+
+const updateCounter = async () => {
+    const data = await fetch('https://api.countapi.xyz/hit/nohamr-counter/error404')
+    const count = await data.json()
+    console.log(count.value)
+    document.getElementById("compteur-de-vues").innerHTML = count.value
+    document.getElementById("compteur-de-vues").style.filter = 'blur(0)'
+};
+
+updateCounter();
